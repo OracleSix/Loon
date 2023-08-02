9f6ea84f3ce7300caf870826702ddf78d98e6e92
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-02 22:57:50 +0800

    更新 CoolApk.js

--- CoolApk.js
+++ CoolApk.js
@@ -2,7 +2,7 @@ let obj = JSON.parse($response.body);
 
 obj.data = obj.data.filter(item => {
 return item.entityTemplate==="feed";
-console("酷安主页已净化🫡");
+console.log("酷安主页已净化🫡");
 });
 //body = body.replace(/extraData/g,"extraDa");//⛔️什么值得买
 
