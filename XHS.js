35524c15ce41eb52eb713f7192e9a17e6c6900b6
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-02 12:32:44 +0800

    Adding a feature that it can removes the watermark of XHS

--- XHS.js
+++ XHS.js
@@ -1,14 +1,16 @@
 //统一获取url和body
 const url = $request.url;
-let body = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 //-----------------------------------
-if (-1 != url.indexOf("homefeed?")) {//⛔️去除主页赞助广告直播
-body.data = body.data.filter(item => {
+if (url.includes("homefeed?")) {//⛔️去除主页赞助广告直播
+obj.data = obj.data.filter(item => {
 return !(item.is_tracking===true||item.is_ads===true||item.live||item.ads_info);
 });
-} else if (-1 != url.indexOf("get_danmaku?")) {//⛔️弹幕净化
-body.data.infos = body.data.infos.filter(item => {return !(/厄运|恶运|噩运|走开|..走./.test(item.content))});
-} else if (-1 != url.indexOf("search/notes?")) {//⛔️去除搜索页广告
-body.data.items = body.data.items.filter(item => {return !(item.ads);});
+} else if (url.includes("get_danmaku?")) {//⛔️弹幕净化
+obj.data.infos = obj.data.infos.filter(item => {return !(/厄运|恶运|噩运|走开|..走./.test(item.content))});
+} else if (url.includes("search/notes?")) {//⛔️去除搜索页广告
+obj.data.items = obj.data.items.filter(item => {return !(item.ads);});
+} else if (url.includes("note/feed")||url.includes("note/redtube")||url.includes("note/videofeed")){
+obj.?data.?note_list.?media_save_config.?disable_watermark=true;
 }
-$done({body:JSON.stringify(body)});
+$done({body:JSON.stringify(obj)});
