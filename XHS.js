8cae51abac039b40db14b9c66ef272a4e4de8787
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-02 19:48:00 +0800

    更新 XHS.js

--- XHS.js
+++ XHS.js
@@ -15,7 +15,7 @@ obj.data.items = obj.data.items.filter(item => {return !(item.ads);});
 console.log("搜索页广告已驱逐🔍");
 } else if (url.includes("note/feed")||url.includes("note/redtube")||url.includes("note/videofeed")){
 //obj?.data?.note_list?.media_save_config?.disable_watermark=true;
-obj = obj.replace('disable_watermark":false"','disable_watermark":true');
+obj = obj.replace('disable_watermark":false','disable_watermark":true');
 console.log(obj);
 console.log("水印已去除🫡");
 };
