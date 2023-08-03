debabfef206361b3412f494ce66ce1af3b78835b
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-03 12:46:09 +0800

    更新 YCCookie.js

--- YCCookie.js
+++ YCCookie.js
@@ -1,5 +1,5 @@
 let obj = JSON.stringify($request.headers);
-let cookie = JSON.parse(obj).cookie
+let cookie = JSON.parse(obj).cookie;
 console.log(cookie);
 if (cookie != $persistentStore.read("YC")) {
 $persistentStore.write(cookie, "YC");
@@ -7,4 +7,4 @@ $notification.post("cookie已更新🎉","","");
 } else {
 $notification.post("无需更新Cookie","","");
 };
-$done({});
+$done();
