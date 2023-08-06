d39776bf2ec79a8310bb3f2bba63d89acbe717ee
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-06 10:49:57 +0800

    Updating it to support removes reply ads

--- CoolApk.js
+++ CoolApk.js
@@ -1,7 +1,14 @@
+const url = $request.url
 let obj = JSON.parse($response.body);
-obj.data = obj.data.filter(item => {
-return item.entityTemplate==="feed";
-});
+
+if (url.includes("main/index")) {
+    obj.data = obj.data.filter(item => {
+    return item.entityTemplate==="feed";
+    });
 //body = body.replace(/extraData/g,"extraDa");//⛔️什么值得买
-console.log("酷安主页已净化🫡");
-$done({body:JSON.stringify(obj)});
+    console.log("酷安主页已净化🫡");
+    $done({body:JSON.stringify(obj)});
+} else if (url.includes("feed/replyList")) {
+    obj = JSON.stringify(obj).replace(/sponsor/g,"spons");
+    $done({body:obj});
+}
