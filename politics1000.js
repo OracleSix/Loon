25904358db79f519cb4a141d126308decfb059b7
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-30 16:08:03 +0800

    Update politics1000.js

--- politics1000.js
+++ politics1000.js
@@ -1,11 +1,11 @@
 const url = $request.url;
+let obj = $request.body;
 
 if (url.includes("unlockCourse")) {
-let obj = JSON.parse($response.body);
+obj = JSON.parse(obj);
 obj = {"errorCode":0,"errorMsg":"ok","stime":1693380623697}
 $done({body:JSON.stringify(obj)});
-} else if (url.includes("user")||url.includes("index")) {
-let obj = $response.body;
+} else if (url.includes("indexInfo")) {
 obj = obj.replace(/upValue":\d*/gi,'upValue":66666');
 //obj = obj.replace(/false/gi,'true');
 $done({body:obj});
