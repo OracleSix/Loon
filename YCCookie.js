efd81e5795030c99f8006befe031bb0a89cac68e
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-08-30 10:19:14 +0800

    Update YCCookie.js

--- YCCookie.js
+++ YCCookie.js
@@ -1,10 +1,11 @@
 let obj = JSON.stringify($request.headers);
 let cookie = JSON.parse(obj).cookie;
-console.log(cookie);
+console.log("🍪本次获取到的cookie:\n" + cookie);
 if (cookie != $persistentStore.read("YC")) {
 $persistentStore.write(cookie, "YC");
 $notification.post("cookie已更新🎉","","");
+$done({});
 } else {
 $notification.post("无需更新Cookie","","");
+$done({});
 };
-$done();
