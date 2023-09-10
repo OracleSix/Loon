2993ddf4ce835e60accccc269ec2c37455b77010
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-09-10 09:59:24 +0800

    Update XHS.js

--- XHS.js
+++ XHS.js
@@ -4,20 +4,20 @@ let obj = $response.body;
 
 if (url.includes("homefeed?")) {
 obj = JSON.parse(obj);//JSON
-//return !(item.is_tracking===true||item.is_ads===true||item.live||item.ads_info);
-obj.data = obj.data.filter(item => {item.is_ads == false;});
-obj.data = obj.data.filter(item => {item.is_tracking == false;});
-console.log("主页广告已屏蔽🥰");
+obj.data = obj.data.filter(item => {
+return !(item.is_tracking===true||item.is_ads===true||item.live||item.ads_info);
+});
+console.log("\n🥰小红书主页广告已屏蔽");
 $done({body:JSON.stringify(obj)});
 } else if (url.includes("get_danmaku?")) {
 obj = JSON.parse(obj);//JSON类型
-obj.data.infos = obj.data.infos.filter(item => {!(/(厄运|恶运|噩运|邪运|走开|..走.)/.test(item.content))});
-console.log("弹幕已净化🤩");
+obj.data.infos = obj.data.infos.filter(item => {return !(/厄运|恶运|噩运|走开|..走./.test(item.content))});
+console.log("\n🤩小红书弹幕已净化");
 $done({body:JSON.stringify(obj)});
 } else if (url.includes("search/notes?")) {
 obj = JSON.parse(obj);//JSON类型
-obj.data.items = obj.data.items.filter(item => {!(item.ads);});
-console.log("搜索页广告已驱逐🔍");
+obj.data.items = obj.data.items.filter(item => {return !(item.ads);});
+console.log("\n🔍小红书搜索页广告已驱逐");
 $done({body:JSON.stringify(obj)});
 } else if (url.includes("splash_config")) {
 // 开屏广告
@@ -34,10 +34,6 @@ obj = JSON.parse(obj);//JSON类型
       }
     });
   }
-console.log("开屏广告已屏蔽🍑");
+console.log("\n🍑小红书开屏广告已屏蔽");
 $done({body:JSON.stringify(obj)});
-}else if (url.includes("note/feed")||url.includes("note/redtube")||url.includes("note/videofeed")) {
-obj = obj.replace('disable_watermark":false','disable_watermark":true');
-console.log("水印已去除🫡");
-$done({body:obj});
 };
