89a9656d924b938297a1869eeaba6cc1e0771198
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-09-10 12:10:38 +0800

    Update XHS.js

--- XHS.js
+++ XHS.js
@@ -36,4 +36,8 @@ obj = JSON.parse(obj);//JSON类型
   }
 console.log("\n🍑小红书开屏广告已屏蔽");
 $done({body:JSON.stringify(obj)});
+} else if (url.includes("note/feed")||url.includes("note/redtube")||url.includes("note/videofeed")) {
+obj = obj.replace('disable_watermark":false','disable_watermark":true');
+console.log("\n水印已去除🫡");
+$done({body:obj});
 };
