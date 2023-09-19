827d1bf60d57a9b2e39e05c0aab9dfebeb58c9c4
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-09-20 07:46:17 +0800

    Update demo2.js

--- demo2.js
+++ demo2.js
@@ -7,6 +7,7 @@ if (url.includes("topstory")) {
     obj = $prefs.valueForKey("topStory");*/
     obj = JSON.parse(obj);
     obj.data = [];
+    obj.fresh_text = ' ';
     $done({body:JSON.stringify(obj)});
 } else if (url.includes("feed")) {
 
@@ -18,4 +19,5 @@ obj = obj.replace(/zhimg/gi,"zhi");
 //placeholder_title":"职场季","img_url":"https://picx.zhimg.com/v2-ce30f9115b882b17b32cddec8e5aacd6.png","img_url_night":"https://pic1.zhimg.com/v2-003f27cec2f5c2daf4e5a75969c18001.png","img_width":60,"img_height":15,"title":"职场季"}
 $done({body:obj});    
 };
+
 
