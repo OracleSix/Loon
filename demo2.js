186f3b02cee11a268f7a88f16914314cfc9d7897
OracleSix <99546786+OracleSix@users.noreply.github.com>
2023-09-20 08:07:42 +0800

    Update demo2.js

--- demo2.js
+++ demo2.js
@@ -18,6 +18,10 @@ obj = obj.replace(/职场季/gi," ");
 obj = obj.replace(/zhimg/gi,"zhi");
 //placeholder_title":"职场季","img_url":"https://picx.zhimg.com/v2-ce30f9115b882b17b32cddec8e5aacd6.png","img_url_night":"https://pic1.zhimg.com/v2-003f27cec2f5c2daf4e5a75969c18001.png","img_width":60,"img_height":15,"title":"职场季"}
 $done({body:obj});    
+} else if (url.includes('preset_words')) {
+    obj = JSON.parse(obj);
+    obj.preset_words = {};
+    $done({body:JSON.stringify(obj)});
 };
 
 
